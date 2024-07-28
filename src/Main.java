a2a59e8850768308e8e3a5d65c605426b7d7b981
OzlamHasanova <7qk9bme@code.edu.az>
2024-07-28 10:44:45 +0400

    upload project and stream methods

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        EmployeeService employeeService = new EmployeeService();
+//        employeeService.getNotEmployeeNameList();
+//        employeeService.getEmployeesFullName();
+//        employeeService.getGroupEmployeesByEmployeeStatus();
+//        employeeService.getEmployeeNamesListBySalary();
+//        employeeService.getITDepartmentEmployees();
+//        employeeService.employeeListBySurname();
+//        employeeService.findEmployeeWithLowestSalary();
+//        employeeService.findEmployeeWithHighestSalary();
+//        employeeService.getTotalSalary();
+//        employeeService.calculateAgeAverageByDepartment();
+//        employeeService.filterEmployeesByAge();
+//        employeeService.getListOfEmployeeNames();
+//        employeeService.calculateAverageSalary();
+//        employeeService.getGroupEmployeesByDepartment();
+//        employeeService.countEmployeesByDepartment();
+//        employeeService.increaseSalary();
+//        employeeService.sumOfSalaryInIT();
+//        employeeService.getDepartmentTotalSalary();
+//        employeeService.getListOfEmployeeInSalaryDescendingOrder();
+        employeeService.getListOfEmployeeInSalaryDescendingOrder();
+
+
+    }
+}
---
+++ src/Position.java
@@ -0,0 +1,4 @@
+public enum Position {
+    INTERN, JUNIOR, SENIOR, MANAGER, DIRECTOR, EXECUTIVE
+
+}
